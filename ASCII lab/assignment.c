f0f50fe976b1fda169a221b875cb5043e0e8f705
Madina <madina.mammadova.std@bhos.edu.az>
2020-11-11 14:17:26 +0400

    added lab

---
+++ ASCII lab/assignment.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+void sort(char *source, char *integers, char *nonIntegers)
+{
+    
+  int i=0;
+  int n=0;
+  int k=0;
+  for( ;source[i]!='\0';i++)
+  { 
+     if (source[i]>=48 && source[i]<=57)
+     {
+	     integers[n]=source[i];
+	     n++;
+     }
+     else 
+     {
+	     nonIntegers[k]=source[i];
+	     k++;
+     }
+  }
+
+}
+
+int main()
+{
+
+char source[] = "abc123def99d#!";
+char integers[256];
+char nonIntegers[256];
+
+sort(source, integers, nonIntegers);
+
+printf("source: %s\nnonintegers: %s\nintegers: %s\n",source, nonIntegers, integers);
+
+return 0;
+}
---
+++ ASCII lab/readme.md
@@ -0,0 +1,3 @@
+## ASCII lab ##
+
+In this assignment,two arrays are created to show integers and non integers in the given character array.
